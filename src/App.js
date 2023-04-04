a4150696ecf27e23aaeedbce3aa88aa90a025eee
Pavel-OOP <pavel.i.petrov.7@gmail.com>
2023-04-04 16:47:07 +0300

    modals and such

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import Modal from "./components/Modal";
 import Todo from "./components/Todo";
+import Backdrop from "./components/Backdrop";
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
       <Todo text="Learn React, bitch!" />
+      <Todo text="Learn Next, bitch!" />
+      <Todo text="Learn Javascript, bitch!" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
